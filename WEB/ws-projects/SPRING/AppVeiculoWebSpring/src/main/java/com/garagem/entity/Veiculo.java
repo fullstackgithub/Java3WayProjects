c728e275d724a77437c3387d5379ac8a103fda1a
Wanderson Lima <fullstackgithub@gmail.com>
2022-08-12 02:33:42 +0100

    Aula 22 - Veiculo, VeiculoRepository, VeiculoService - Spring

---
+++ WEB/ws-projects/SPRING/AppVeiculoWebSpring/src/main/java/com/garagem/entity/Veiculo.java
@@ -0,0 +1,71 @@
+package com.garagem.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "veiculo")
+public class Veiculo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String marca;
+	private String modelo;
+	private String descricao;
+	private String placa;
+	private int ano;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+}
---
+++ WEB/ws-projects/SPRING/AppVeiculoWebSpring/src/main/java/com/garagem/service/VeiculoService.java
@@ -0,0 +1,35 @@
+package com.garagem.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.garagem.entity.Veiculo;
+import com.garagem.repository.VeiculoRepository;
+
+@Service
+public class VeiculoService {
+
+	@Autowired
+	private VeiculoRepository repository;
+
+	public void saveOrUpdate(Veiculo veiculo) {
+		repository.save(veiculo);
+
+	}
+
+	public void deleteById(Long id) {
+		repository.deleteById(id);
+	}
+
+	public List<Veiculo> findAll() {
+		return repository.findAll();
+	}
+
+	public Optional<Veiculo> findById(Long id) {
+		return repository.findById(id);
+	}
+
+}
