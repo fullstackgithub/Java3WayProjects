9e536203746aab24ac35c442a106db060dc4b829
Wanderson Lima <fullstackgithub@gmail.com>
2022-08-16 20:47:05 +0100

    Aula 25 - VeiculoController - Editar e Excluir

--- WEB/ws-projects/SPRING/AppVeiculoWebSpring/src/main/java/com/garagem/controller/VeiculoController.java
+++ WEB/ws-projects/SPRING/AppVeiculoWebSpring/src/main/java/com/garagem/controller/VeiculoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -12,25 +13,46 @@ import com.garagem.service.VeiculoService;
 
 @Controller
 public class VeiculoController {
-	
+
 	@Autowired
-	VeiculoService service; 
-	
+	VeiculoService service;
+
 	@GetMapping("/novo")
 	public String cadastrar(Veiculo objVeiculo) {
 		return "frmveiculo";
 	}
-	
+
 	@GetMapping("/listar")
 	public String listar(ModelMap model) {
 		model.addAttribute("lstVeiculos", service.findAll());
 		return "lstveiculos";
 	}
-	
+
 	@PostMapping("/salvar")
 	public String salvar(Veiculo objVeiculo, RedirectAttributes attr) {
 		service.saveOrUpdate(objVeiculo);
 		attr.addFlashAttribute("msg", "Veículo inserido com sucesso!");
 		return "redirect:/novo";
+	}
+
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Veiculo objVeiculo, ModelMap model) {
+		model.addAttribute("objVeiculo", objVeiculo);
+		return "frmveiculo";
+	}
+
+	@PostMapping("/editar")
+	public String editar(Veiculo objVeiculo, RedirectAttributes attr) {
+		service.saveOrUpdate(objVeiculo);
+		attr.addFlashAttribute("msg", "Veículo alterado com sucesso!");
+		return "redirect:/novo";
+
+	}
+
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable("id") Long id, ModelMap model) {
+		service.deleteById(id);
+		model.addAttribute("msg", "Veículo excluído com sucesso!");
+		return listar(model);
 	}
 }
