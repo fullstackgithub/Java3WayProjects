f6b9d8f03eaada5a96894bbda57a9637a264fc96
Wanderson Lima <fullstackgithub@gmail.com>
2022-08-16 21:56:54 +0100

    Aula 25 - Adding Lombok

--- WEB/ws-projects/SPRING/AppVeiculoWebSpring/src/main/java/com/garagem/entity/Veiculo.java
+++ WEB/ws-projects/SPRING/AppVeiculoWebSpring/src/main/java/com/garagem/entity/Veiculo.java
@@ -6,6 +6,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "veiculo")
 public class Veiculo {
@@ -19,53 +28,5 @@ public class Veiculo {
 	private String descricao;
 	private String placa;
 	private int ano;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getMarca() {
-		return marca;
-	}
-
-	public void setMarca(String marca) {
-		this.marca = marca;
-	}
-
-	public String getModelo() {
-		return modelo;
-	}
-
-	public void setModelo(String modelo) {
-		this.modelo = modelo;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-
-	public String getPlaca() {
-		return placa;
-	}
-
-	public void setPlaca(String placa) {
-		this.placa = placa;
-	}
-
-	public int getAno() {
-		return ano;
-	}
-
-	public void setAno(int ano) {
-		this.ano = ano;
-	}
 
 }
